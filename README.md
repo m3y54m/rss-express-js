81cdcaca2aee962b4435fbd86c14c7723c32e425
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-11-10 23:34:40 +0330

    View RSS as an HTML page using Express and Pug

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # RSS Feed Viewer Using Express.js
 
-## Project Initialization
+## Initialization
 
 Install the following NodeJS packages:
 
 ```console
 yarn add rss-parser
+yarn add express
+yarn add pug
 ```
 
+## Run
+
+```console
+node app.js
+```
+
+## Resources
+
+- [rss-parser](https://www.npmjs.com/package/rss-parser)
+- [Using template engines with Express](https://expressjs.com/en/guide/using-template-engines.html)
+- [Pug - Iteration](https://pugjs.org/language/iteration.html)
--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-let Parser = require('rss-parser');
-let parser = new Parser();
+// Express HTTP server
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+// Pug template engine for displaying variables in a HTML page
+const pug = require("pug");
+// Set "Pug" as a template engine for Express
+app.set("view engine", "pug");
+// RSS Parser
+const Parser = require("rss-parser");
+const parser = new Parser();
 
-(async () => {
+app.get("/", async (req, res) => {
 
-  let feed = await parser.parseURL('https://virgool.io/feed/@virgool');
-  console.log(feed.title);
+  let feed = await parser.parseURL("https://virgool.io/feed/@virgool");
+  var feedItems = [];
 
-  feed.items.forEach(item => {
-    console.log(item.title + ':' + item.link)
+  feed.items.forEach((item) => {
+    feedItems.push({
+      title: item.title,
+      link: item.link,
+    });
   });
 
-})();
+  // The template for index page is in "views/index.pug"
+  res.render("index", {
+    feedItems: feedItems,
+  });
+});
+
+// Run Express server
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
